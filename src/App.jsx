6ce31cefb2b40728d2fc61ff61a95c1fecb52e3d
jutin0852 <jutindikonu8@gmail.com>
2025-06-05 20:03:52 +0100

    model fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import Feature from "./component/Feature";
 import Hero from "./component/Hero";
 import Highlights from "./component/Highlights";
 import HowItWorks from "./component/HowItWorks";
-import Model from "./component/Model";
 import Navbar from "./component/Navbar";
+import Model from './component/Model'
+
 
 function App() {
   return (
